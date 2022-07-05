0f1a689456fcaaef6519468147b3d4ed076c03d1
jocnn <ijann_@outlook.com>
2022-07-05 14:03:37 -0300

    se creó el layout del proyecto

--- components/layout.js
+++ components/layout.js
@@ -5,9 +5,19 @@ const Layout = ({ children }) => {
   return (
 		<>
       <Head>
+				<meta
+					name='viewport'
+					content='initial-scale=1.0, width=device-width'
+				/>
         <title>NodeSend</title>
 			</Head>
-			{children}
+			<div className='bg-gray-100 min-h-screen'>
+				<div className='container mx-auto'>
+          <main className='mt-20'>
+            {children}
+          </main>
+				</div>
+			</div>
 		</>
   )
 }
