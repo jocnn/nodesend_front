3781e7f1ebe0a4937c54d61799ef2454454480ef
jocnn <ijann_@outlook.com>
2022-07-05 17:30:15 -0300

    se creó la página de registro

---
+++ pages/signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const SignIn = () => {
+  return (
+    <Layout>
+      <h1>SignIn</h1>
+    </Layout>
+  )
+}
+
+export default SignIn
