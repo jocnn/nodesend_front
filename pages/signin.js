87749ac6854a2db20c6b2afabf1ef6de42e6b6d3
jocnn <ijann_@outlook.com>
2022-07-06 07:26:34 -0300

    se diseño el formulario de registro

--- pages/signin.js
+++ pages/signin.js
@@ -2,11 +2,68 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const SignIn = () => {
-  return (
-    <Layout>
-      <h1>SignIn</h1>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<div className='md:w-4/5 xl:w-3/5 mx-auto mb-32'>
+				<h2 className='text-4xl font-sans font-bold text-gray-800 text-center my-4'>
+					Crear Cuenta
+				</h2>
+				<div className='flex justify-center mt-5'>
+					<div className='w-full max-w-lg'>
+						<form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'>
+							<div className='mb-4'>
+								<label
+									className='block text-black text-sm font-bold mb-2'
+									htmlFor='name'>
+									Nombre
+								</label>
+								<input
+									id='name'
+									type='text'
+									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+									placeholder='Nombre de usuario'
+								/>
+							</div>
+
+							<div className='mb-4'>
+								<label
+									className='block text-black text-sm font-bold mb-2'
+									htmlFor='email'>
+									Correo Electrónico
+								</label>
+								<input
+									id='email'
+									type='email'
+									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+									placeholder='Correo Electrónico de tu usuario'
+								/>
+							</div>
+
+							<div className='mb-4'>
+								<label
+									className='block text-black text-sm font-bold mb-2'
+									htmlFor='password'>
+									Contraseña
+								</label>
+								<input
+									id='password'
+									type='password'
+									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+									placeholder='Contraseña de usuario'
+								/>
+							</div>
+
+							<input
+								type='submit'
+								value='Crear Cuenta'
+								className='bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold cursor-pointer'
+							/>
+						</form>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	)
 }
 
 export default SignIn
