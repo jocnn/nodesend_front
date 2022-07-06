9347d35a1f02bd5899cc520a3a1b186873527c6e
jocnn <ijann_@outlook.com>
2022-07-06 09:20:08 -0300

    usando formik y yup en el formulario de registro

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const SignIn = () => {
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			password: '',
+		},
+		validationSchema: Yup.object({
+			name: Yup
+				.string()
+				.required('El nombre es obligatorio'),
+			email: Yup
+				.string()
+				.email('El correo no es válido')
+				.required('El correo es obligatorio'),
+			password: Yup
+				.string()
+				.required('La contraseña no puede ir vacia')
+				.min(6, 'La contraseña debe contener al menos 6 caracteres'),
+		}),
+		onSubmit: (values) => {
+			console.log('Enviando formulario', values)
+		},
+	})
+
 	return (
 		<Layout>
 			<div className='md:w-4/5 xl:w-3/5 mx-auto mb-32'>
@@ -10,7 +36,9 @@ const SignIn = () => {
 				</h2>
 				<div className='flex justify-center mt-5'>
 					<div className='w-full max-w-lg'>
-						<form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'>
+						<form
+							className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'
+							onSubmit={formik.handleSubmit}>
 							<div className='mb-4'>
 								<label
 									className='block text-black text-sm font-bold mb-2'
@@ -22,7 +50,17 @@ const SignIn = () => {
 									type='text'
 									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
 									placeholder='Nombre de usuario'
+									value={formik.values.name}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									autoComplete="name"
 								/>
+								{formik.touched.name && formik.errors.name ? (
+									<div className='my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4'>
+										<p className='font-bold'>Error</p>
+										<p>{formik.errors.name}</p>
+									</div>
+								) : null}
 							</div>
 
 							<div className='mb-4'>
@@ -36,7 +74,17 @@ const SignIn = () => {
 									type='email'
 									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
 									placeholder='Correo Electrónico de tu usuario'
+									value={formik.values.email}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									autoComplete="email"
 								/>
+								{formik.touched.email && formik.errors.email ? (
+									<div className='my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4'>
+										<p className='font-bold'>Error</p>
+										<p>{formik.errors.email}</p>
+									</div>
+								) : null}
 							</div>
 
 							<div className='mb-4'>
@@ -50,7 +98,18 @@ const SignIn = () => {
 									type='password'
 									className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
 									placeholder='Contraseña de usuario'
+									value={formik.values.password}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									autoComplete="current-password"
 								/>
+								{formik.touched.password &&
+								formik.errors.password ? (
+									<div className='my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4'>
+										<p className='font-bold'>Error</p>
+										<p>{formik.errors.password}</p>
+									</div>
+								) : null}
 							</div>
 
 							<input
