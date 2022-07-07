747fe1bac193d8ff81e8a52aba6555937283094a
jocnn <ijann_@outlook.com>
2022-07-07 15:56:15 -0300

    se definió el registro del usuario en el provider

--- context/auth/authContext.js
+++ context/auth/authContext.js
@@ -1,33 +1,56 @@
 import { createContext, useReducer } from 'react'
 import authReducer from './authReducer'
+import clientAxios from '../../config/clientAxios'
 
-import { USER_AUTHENTICATE } from '../types'
+import {
+	USER_AUTHENTICATE,
+	REGISTER_SUCCESS,
+	REGISTER_FAILED,
+	CLEANING_ALERT_MESSAGE,
+} from '../types'
 
 const AuthContext = createContext()
 
 const initialState = {
-  token: '',
-  authenticated: null,
-  user: null,
-  message: null,
+	token: '',
+	authenticated: null,
+	user: null,
+	message: null,
 }
 
 const AuthProvider = ({ children }) => {
-
 	const [state, dispatch] = useReducer(authReducer, initialState)
 
-  const registerUser = data => {
-    console.log('desde registerUser')
-  }
+	const registerUser = async (data) => {
+		try {
+			const resp = await clientAxios.post('/api/users', data)
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: resp.data.msg,
+			})
+		} catch (error) {
+			console.log(error)
+			dispatch({
+				type: REGISTER_FAILED,
+				payload: error.response.data.msg,
+			})
+		} finally {
+			setTimeout(() => {
+				dispatch({
+					type: CLEANING_ALERT_MESSAGE,
+				})
+			}, 3000)
+		}
+	}
 
-  const userAuthenticated = name => {
-    dispatch({
-      type: USER_AUTHENTICATE,
-      payload: name,
-    })
-  }
+	const userAuthenticated = (name) => {
+		dispatch({
+			type: USER_AUTHENTICATE,
+			payload: name,
+		})
+	}
 
-  return (
+	return (
 		<AuthContext.Provider
 			value={{
 				token: state.token,
@@ -39,7 +62,7 @@ const AuthProvider = ({ children }) => {
 			}}>
 			{children}
 		</AuthContext.Provider>
-  )
+	)
 }
 
 export { AuthProvider }
--- context/auth/authReducer.js
+++ context/auth/authReducer.js
@@ -1,12 +1,32 @@
-import { USER_AUTHENTICATE } from '../types'
+import {
+	USER_AUTHENTICATE,
+	REGISTER_SUCCESS,
+	REGISTER_FAILED,
+	CLEANING_ALERT_MESSAGE,
+} from '../types'
 
 const authReducer = (state, action) => {
-  switch (action.type) {
-    case USER_AUTHENTICATE:
-      return {
-        ...state,
-        user: action.payload,
-      }
+	switch (action.type) {
+		case USER_AUTHENTICATE:
+			return {
+				...state,
+				user: action.payload,
+			}
+		case REGISTER_SUCCESS:
+			return {
+				...state,
+				message: action.payload,
+			}
+		case REGISTER_FAILED:
+			return {
+				...state,
+				message: action.payload,
+			}
+		case CLEANING_ALERT_MESSAGE:
+			return {
+				...state,
+				message: null,
+			}
 		default:
 			return state
 	}
