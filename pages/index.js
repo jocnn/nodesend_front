1e685ef1b694c9426239b732d031b794b00d7664
jocnn <ijann_@outlook.com>
2022-07-09 15:34:20 -0300

    se ejecuta la autenticación solo una vez

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../components/layout'
+import useAuth from '../context/auth/authState'
+
+const authenticated = value => value
 
 const Index = () => {
+
+	const { userAuthenticated } = useAuth()
+	authenticated(userAuthenticated())
+
+	useEffect(() => {
+		authenticated()
+	}, [])
+
   return (
 		<Layout>
 			<h1>Index</h1>
