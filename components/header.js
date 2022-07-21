9ddd261c26830f11d9066a90342e8bbc87f40f1d
jocnn <ijann_@outlook.com>
2022-07-20 21:47:29 -0300

    se condiciono el header al iniciar sesión

--- components/header.js
+++ components/header.js
@@ -1,32 +1,53 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import useAuth from '../context/auth/authState'
 
 const Header = () => {
+
+  const { user, userAuthenticated, userLogout } = useAuth()
+
+	useEffect(() => {
+		userAuthenticated()
+	}, [userAuthenticated])
+
   return (
-    <header className='py-4 flex flex-col md:flex-row items-center justify-between'>
-      <Link href="/">
-        <a>
-          <Image
-            className='w-64 mb-8 md:mb-0'
-            width={150}
-            height={100}
-            src='/logo.svg'
-            alt='logo de la página'
-          />
-        </a>
-      </Link>
+		<header className='py-4 flex flex-col md:flex-row items-center justify-between'>
+			<Link href='/'>
+				<a>
+					<Image
+						className='w-64 mb-8 md:mb-0'
+						width={150}
+						height={100}
+						src='/logo.svg'
+						alt='logo de la página'
+					/>
+				</a>
+			</Link>
 			<div className='flex gap-2'>
-				<Link href='/login'>
-					<a className='bg-red-500 px-5 py-3 rounded text-white font-bold uppercase'>
-						Iniciar Sesión
-					</a>
-				</Link>
-				<Link href='/signin'>
-					<a className='bg-black px-5 py-3 rounded text-white font-bold uppercase'>
-						Crear Cuenta
-					</a>
-				</Link>
+				{user ? (
+					<div className='flex items-center gap-2'>
+						<p>Hola: {user.name}</p>
+						<button
+							className='bg-black px-5 py-3 rounded text-white font-bold uppercase'
+							onClick={ () => userLogout() }>
+							Cerrar Sesión
+						</button>
+					</div>
+				) : (
+					<>
+						<Link href='/login'>
+							<a className='bg-red-500 px-5 py-3 rounded text-white font-bold uppercase'>
+								Iniciar Sesión
+							</a>
+						</Link>
+						<Link href='/signin'>
+							<a className='bg-black px-5 py-3 rounded text-white font-bold uppercase'>
+								Crear Cuenta
+							</a>
+						</Link>
+					</>
+				)}
 			</div>
 		</header>
   )
